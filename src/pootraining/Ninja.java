c3accb4c7c8da5731d497fd25a055b948ff865ad
Gustavo Dos Santos Pereira <pereira.worker@gmail.com>
2026-05-04 19:01:49 -0300

    Alterado os atribudos de publico para privado - criado constructor para facilidar - Getters e Setters para os atributos privados - 2 Metodos globais (atacar, defender)

--- src/pootraining/Ninja.java
+++ src/pootraining/Ninja.java
@@ -1,7 +1,59 @@
 package pootraining;
 //Classe mae
 public class Ninja {
-    public String nome;
-    public int idade;
-    public String aldeia;
+//    =========
+//    Atributos
+//    =========
+    private String nome;
+    private int idade;
+    private String aldeia;
+//    ============
+//    Constructor
+//    ===========
+    public Ninja(String nome, int idade, String aldeia){
+        this.nome = nome;
+        this.idade = idade;
+        this.aldeia = aldeia;
+    }
+
+//    =================
+//    Getters e Setters
+//    =================
+
+    public String getNome(){
+        return nome;
+    };
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public int getIdade(){
+        return idade;
+    };
+
+    public void setIdade(int idade){
+        this.idade = idade;
+    }
+
+    public String getAldeia(){
+        return aldeia;
+    };
+
+    public void setAldeia(String aldeia){
+        this.aldeia = aldeia;
+    }
+
+//    =================
+//    Metodo Global
+//    =================
+
+    public void atacar(){
+        System.out.println(nome + "Atacou!");
+    }
+
+    public void defender(){
+        System.out.println(nome + "Defendeu!");
+    }
+
 }
